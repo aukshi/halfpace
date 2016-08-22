90c3423ea30adf568df38f4a0e037b08477a2ba2
Ajinkya Gurav <Ajinkya Gurav@192.168.137.54>
2016-08-22 10:51:05 +0530

    new commit

--- forgot-password.php
+++ forgot-password.php
@@ -73,8 +73,8 @@ include 'DB_Con.php';
                                 $mail->isSMTP();                            // Set mailer to use SMTP
                                 $mail->Host = "smtp.gmail.com";             // Specify main and backup SMTP servers
                                 $mail->SMTPAuth = true;                     // Enable SMTP authentication
-                                $mail->Username = 'ajinkyagurav21695@gmail.com';          // SMTP username
-                                $mail->Password = '121237h21'; // SMTP password
+                                $mail->Username = '.com';          // SMTP username
+                                $mail->Password = ''; // SMTP password
                                 $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
                                 $mail->Port = 587;                          // TCP port to connect to
                                 $mail->setFrom('ajinkyagurav21695@gmail.com','Ajinkya Gurav');
