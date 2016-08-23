e1a7c38fa25b3ee4d9c2f2325c1ecf60ce77dedd
User <User@User-PC>
2016-08-23 16:08:49 +0530

    Merge origin/master
    Conflicts:
    	index.php

--- try.php
+++ try.php
@@ -10,6 +10,12 @@
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
   <script src="js/checkbox.js"></script>
+  
+  
+  
+  
+  
+  
   <script>
 function myFunction(id) {
     $(".drop").hide();
@@ -23,9 +29,11 @@ function myFunction(id) {
 </script>
 </head>
 <body>
+   
+   
     
     <div class="superparent" id="left_div">
-        <h2>Choose a skill</h2>
+        <h2>Choose your skill</h2>
  
 <div class="w3-accordion w3-light-grey">
   <button onclick="myFunction('Demo1')" class="w3-btn-block w3-left-align">
@@ -42,9 +50,9 @@ function myFunction(id) {
                       foreach($skills as $item){
                       ?>
                                 <div class="squaredOne">
-                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared[$i];?>" name="check" />
+                                   <input class="cbox" type="checkbox" value="None" id="<?php echo $squared[$i];?>" name="check" />
                                     <label for="<?php echo $squared[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
-                                   <div class="drop w3-card-8 w3-dropdown-content" >
+                                    <div class="drop w3-card-8 w3-dropdown-content" >
                                     
                           <div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
                               <label class="w3-validate">Beginner</label></div>
@@ -225,7 +233,7 @@ function myFunction(id) {
         
         
         
-         <button onclick="myFunction('Demo6')" class="w3-btn-block w3-left-align">
+        <button onclick="myFunction('Demo6')" class="w3-btn-block w3-left-align" >
     Games
   </button>
     <div id="Demo6" class="w3-accordion-content w3-container">
@@ -268,7 +276,7 @@ function myFunction(id) {
     
     <div class="superparent" id="right_div" >
           
-          <h2>I want to learn</h2>
+          <h2>Interests/Requirements</h2>
  
 <div class="w3-accordion w3-light-grey">
   <button onclick="myFunction('Demo11')" class="w3-btn-block w3-left-align">
@@ -299,8 +307,7 @@ function myFunction(id) {
                                           <label class="w3-validate">Expert</label></div>
                                     </div-->
                                     
-                                    
-                                </div-->
+                                </div> 
                       <?php
                       $i++;
                       }
@@ -309,22 +316,22 @@ function myFunction(id) {
                      
                   </div>
   </div>
-  <button onclick="myFunction('Demo2')" class="w3-btn-block w3-left-align">
+  <button onclick="myFunction('Demo22')" class="w3-btn-block w3-left-align">
     Education
   </button>
-  <div id="Demo2" class="w3-accordion-content w3-container">
+  <div id="Demo22" class="w3-accordion-content w3-container">
     <div class="content">
                           
                       <?php
-                      $skills1 = array("General Mathematics","Statestics","Calculus","Chemistry","Physics","Biology","History","Geography","Economics","Civics","Politics","Psychology","Philosophy","Engineering Graphics","Mechanics","C","C++","Java","Python","Web Developement","Mobile App Developement","Embedded System","Microcontroller Programming","Robotics","Compititive Exams","Interviews","Career Guidance","College Selection");
-                      $squared1 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8","squared9","squared10","squared11","squared12","squared13","squared14","squared15","squared16","squared17","squared18","squared19","squared20","squared21","squared22","squared23","squared24","squared25","squared26","squared27","squared28");
+                      $skills22 = array("General Mathematics","Statestics","Calculus","Chemistry","Physics","Biology","History","Geography","Economics","Civics","Politics","Psychology","Philosophy","Engineering Graphics","Mechanics","C","C++","Java","Python","Web Developement","Mobile App Developement","Embedded System","Microcontroller Programming","Robotics","Compititive Exams","Interviews","Career Guidance","College Selection");
+                      $squared22 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8","squared9","squared10","squared11","squared12","squared13","squared14","squared15","squared16","squared17","squared18","squared19","squared20","squared21","squared22","squared23","squared24","squared25","squared26","squared27","squared28");
                       $i=0;
                       foreach($skills1 as $item){
                       ?>
                                 <div class="squaredOne">
-                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared1[$i];?>" name="check" />
-                                    <label for="<?php echo $squared1[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
-                                   <div class="drop w3-card-8 w3-dropdown-content" >
+                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared22[$i];?>" name="check" />
+                                    <label for="<?php echo $squared22[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
+                                   <!--div class="drop w3-card-8 w3-dropdown-content" >
                                     
                           <div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
                               <label class="w3-validate">Beginner</label></div>
@@ -334,7 +341,7 @@ function myFunction(id) {
 
                                       <div> <input class="w3-radio" type="radio" name="gender" value="Expert">
                                           <label class="w3-validate">Expert</label></div>
-                                    </div>
+                                    </div-->
                                     
                                     
                                 </div>
@@ -348,22 +355,22 @@ function myFunction(id) {
   </div>
 </div>
 
-        <button onclick="myFunction('Demo3')" class="w3-btn-block w3-left-align">
+        <button onclick="myFunction('Demo33')" class="w3-btn-block w3-left-align">
     Languages
   </button>
-        <div id="Demo3" class="w3-accordion-content w3-container">
+        <div id="Demo33" class="w3-accordion-content w3-container">
             <div class="content">
                                                  
                       <?php
-                      $skills2 = array("English","Germen","French","Spanish","Hindi","Marathi","Sanskrit","Grammer");
-                      $squared2 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8");
+                      $skills33 = array("English","Germen","French","Spanish","Hindi","Marathi","Sanskrit","Grammer");
+                      $squared33 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8");
                       $i=0;
-                      foreach($skills2 as $item){
+                      foreach($skills33 as $item){
                       ?>
                                 <div class="squaredOne">
-                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared2[$i];?>" name="check" />
-                                    <label for="<?php echo $squared2[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
-                                   <div class="drop w3-card-8 w3-dropdown-content" >
+                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared33[$i];?>" name="check" />
+                                    <label for="<?php echo $squared33[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
+                                   <!--div class="drop w3-card-8 w3-dropdown-content" >
                                     
                           <div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
                               <label class="w3-validate">Beginner</label></div>
@@ -373,7 +380,7 @@ function myFunction(id) {
 
                                       <div> <input class="w3-radio" type="radio" name="gender" value="Expert">
                                           <label class="w3-validate">Expert</label></div>
-                                    </div>
+                                    </div-->
                                     
                                     
                                 </div>
@@ -386,24 +393,24 @@ function myFunction(id) {
                   </div>
         
     
-     <button onclick="myFunction('Demo4')" class="w3-btn-block w3-left-align">
+     <button onclick="myFunction('Demo44')" class="w3-btn-block w3-left-align">
     Food Fitness and Healthcare
   </button>
-    <div id="Demo4" class="w3-accordion-content w3-container">
+    <div id="Demo44" class="w3-accordion-content w3-container">
         <div class="content">
                                                                      
                                     <?php
-                      $skills3 = array("Heart Care","Diabetes Care","Yoga","Diet","Home Remidies","BabyCare","Skin Care","Aerobics","Hair Care","Dressing","Mother care");
-                      $squared3 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8","squared9","squared7","squared8","squared9","squared10","squared11");
+                      $skills44 = array("Heart Care","Diabetes Care","Yoga","Diet","Home Remidies","BabyCare","Skin Care","Aerobics","Hair Care","Dressing","Mother care");
+                      $squared44 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8","squared9","squared7","squared8","squared9","squared10","squared11");
                       $i=0;
-                      foreach($skills3 as $item){
+                      foreach($skills44 as $item){
                       ?>
                                 <div class="squaredOne">
-                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared3[$i];?>" name="check" />
-                                    <label for="<?php echo $squared3[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
-                                   <div class="drop w3-card-8 w3-dropdown-content" >
+                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared44[$i];?>" name="check" />
+                                    <label for="<?php echo $squared44[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
+<!--                                   <div class="drop w3-card-8 w3-dropdown-content" >
                                     
-                          <div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
+                          div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
                               <label class="w3-validate">Beginner</label></div>
 
                                   <div> <input class="w3-radio" type="radio" name="gender" value="Intermediate">
@@ -411,39 +418,41 @@ function myFunction(id) {
 
                                       <div> <input class="w3-radio" type="radio" name="gender" value="Expert">
                                           <label class="w3-validate">Expert</label></div>
-                                    </div>
+                                    </div
                                     
                                     
-                                </div>
+                                </div>-->
+</div>
                       <?php
                       $i++;
                       }
                       ?>
                       
-                  </div>
+                  
                   
         
     </div>
+    </div>
  
         
-         <button onclick="myFunction('Demo5')" class="w3-btn-block w3-left-align">
+         <button onclick="myFunction('Demo55')" class="w3-btn-block w3-left-align">
     Art
   </button>
-    <div id="Demo5" class="w3-accordion-content w3-container">
+    <div id="Demo55" class="w3-accordion-content w3-container">
         <div class="content">
                                                                      
                                     <?php
-                      $skills4 = array("Sketching","Painting","3-D Art","Nail Art","Dance","Singing","Drama","Guitar","Tabla","Synthesiser","Quilling","Card Making","Handcraft");
-                      $squared4 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8","squared9","squared7","squared8","squared9","squared10","squared11");
+                      $skills55 = array("Sketching","Painting","3-D Art","Nail Art","Dance","Singing","Drama","Guitar","Tabla","Synthesiser","Quilling","Card Making","Handcraft");
+                      $squared55 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7","squared8","squared9","squared7","squared8","squared9","squared10","squared11");
                       $i=0;
-                      foreach($skills4 as $item){
+                      foreach($skills55 as $item){
                       ?>
                                 <div class="squaredOne">
-                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared4[$i];?>" name="check" />
-                                    <label for="<?php echo $squared4[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
-                                   <div class="drop w3-card-8 w3-dropdown-content" >
+                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared55[$i];?>" name="check" />
+                                    <label for="<?php echo $squared55[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
+<!--                                   <div class="drop w3-card-8 w3-dropdown-content" >
                                     
-                          <div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
+                          div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
                               <label class="w3-validate">Beginner</label></div>
 
                                   <div> <input class="w3-radio" type="radio" name="gender" value="Intermediate">
@@ -451,9 +460,10 @@ function myFunction(id) {
 
                                       <div> <input class="w3-radio" type="radio" name="gender" value="Expert">
                                           <label class="w3-validate">Expert</label></div>
-                                    </div>
+                                    </div
                                     
                                     
+                                </div>-->
                                 </div>
                       <?php
                       $i++;
@@ -464,26 +474,26 @@ function myFunction(id) {
                   
         
     </div>
- 
+  
         
         
         
-         <button onclick="myFunction('Demo6')" class="w3-btn-block w3-left-align">
-    Games
+         <button onclick="myFunction('Demo66')" class="w3-btn-block w3-left-align">
+    Sports and Games
   </button>
-    <div id="Demo6" class="w3-accordion-content w3-container">
+    <div id="Demo66" class="w3-accordion-content w3-container">
         <div class="content">
                                                                      
                                     <?php
-                      $skills5 = array("Cricket","Badminton","Tennise","Table Tennise","Hockey","Chess","Kabaddi");
-                      $squared5 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7");
+                      $skills66 = array("Cricket","Badminton","Tennise","Table Tennise","Hockey","Chess","Kabaddi");
+                      $squared66 = array("squared1","squared2","squared3","squared4","squared5","squared6","squared7");
                       $i=0;
-                      foreach($skills5 as $item){
+                      foreach($skills66 as $item){
                       ?>
                                 <div class="squaredOne">
-                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared5[$i];?>" name="check" />
-                                    <label for="<?php echo $squared5[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
-                                   <div class="drop w3-card-8 w3-dropdown-content" >
+                                    <input class="cbox" type="checkbox" value="None" id="<?php echo $squared66[$i];?>" name="check" />
+                                    <label for="<?php echo $squared66[$i];?>"><font size="3"> <?php echo $item;?> </font></label>
+                                   <!--div class="drop w3-card-8 w3-dropdown-content" >
                                     
                           <div ><input class="w3-radio" type="radio" name="gender" value="Beginner" checked>
                               <label class="w3-validate">Beginner</label></div>
@@ -493,7 +503,7 @@ function myFunction(id) {
 
                                       <div> <input class="w3-radio" type="radio" name="gender" value="Expert">
                                           <label class="w3-validate">Expert</label></div>
-                                    </div>
+                                    </div-->
                                     
                                     
                                 </div>
@@ -509,7 +519,7 @@ function myFunction(id) {
  
         
          
-         </div>
+        
 </body>
 
 </html>
