9a4d66fb333213533f3fc194af8d3bbde4140053
Ajinkya Gurav <Ajinkya Gurav@192.168.137.54>
2016-08-27 16:45:16 +0530

    Merge origin/master
    Conflicts:
    	friendsprof.php

--- friendsStatus.php
+++ friendsStatus.php
@@ -21,11 +21,19 @@ if (isset($_GET['Acc'])) {
 
 
 function accept(mysqli $con) {
+    $flist=$_SESSION['flist'];
+    $rlist=$_SESSION['rlist'];
     $loggedUser=$_SESSION["loggedUser"];
     $requestedUser=$_GET["requested"];
     echo "<script type='text/javascript'>alert('In Accept '.$requestedUser)</script>";
     $acceptQuery="update friend_status set accepted=1,requested=0 where eid1='$requestedUser' AND eid2='$loggedUser'";
     mysqli_query($con,$acceptQuery);
+    
+    
+    $_SESSION['flist'][]=$requestedUser;//to add currently accepted user into friends' list
+    $_SESSION['rlist']=array_diff($rlist,$requestedUser);//To delete currently accepted user from requested list
+    print_r($_SESSION['rlist']);
+    //var_dump($_SESSION);
     //Enter the action in activity table
     $desc="You Accepted the friend request of $requestedUser";
     $description=mysql_real_escape_string($desc);
@@ -40,12 +48,14 @@ exit;
 }
 
 function decline(mysqli $con) {
-   $loggedUser=$_SESSION["loggedUser"];
+    $flist=$_SESSION['flist'];
+    $rlist=$_SESSION['rlist'];
+    $loggedUser=$_SESSION["loggedUser"];
     $requestedUser=$_GET["requested"];
     echo "<script type='text/javascript'>alert('In Accept '.$requestedUser)</script>";
     $acceptQuery="update friend_status set accepted=0,requested=0 where eid1='$requestedUser' AND eid2='$loggedUser'";
     mysqli_query($con,$acceptQuery);
-    
+    $_SESSION['rlist']=array_diff($rlist,$requestedUser);//To delete declined user from requested list
     //Enter the action in activity table
     $desc="You declined the friend request of $requestedUser";
     $description=mysql_real_escape_string($desc);
@@ -63,6 +73,8 @@ function connect(mysqli $con) {
     echo "<script type='text/javascript'>alert('In connect '.$connect)</script>";
     $acceptQuery="insert into friend_status values('$loggedUser','$connect',1,0)";
     mysqli_query($con,$acceptQuery);
+    $_SESSION['rlist'][]=$connect;//to add currently requested user into requests' list
+    print_r($_SESSION['rlist']);
     //Enter the action in activity table
     $desc="You sent friend request to $connect";
     $description=mysql_real_escape_string($desc);
