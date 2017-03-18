87bb75b0d929517f4d5c1617bccc582e9cd920d2
hp <hp@hp-HP.mshome.net>
2017-03-18 15:01:47 +0530

    Merge origin/master

--- rate.php
+++ rate.php
@@ -3,94 +3,92 @@
         <link rel="stylesheet" href="css/rate.css" 
         <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
         <script type="text/javascript">
-//            var clicked_val=0;
-//            
-            
-           
-                
-                
-                
-//                  $('#s1').hover(function{
+//            var star = getElementByID('s1');
+//            star.src='Images/gstar.png';
+//    $(document).ready(function(){
+//    var clicked_val=0;
+//                  $('#s1').on('mouseover',function(){
 //                      
 //                          $('#s1').attr('src','Images/gstar.png');
-//                           $('#s2').attr('src','Images/estar.png');
-//                            $('#s3').attr('src','Images/estar.png');
-//                             $('#s4').attr('src','Images/estar.png');
-//                              $('#s5').attr('src','Images/estar.png');
+//                           $('#s2').attr('src','Images/estar.JPG');
+//                            $('#s3').attr('src','Images/estar.JPG');
+//                             $('#s4').attr('src','Images/estar.JPG');
+//                              $('#s5').attr('src','Images/estar.JPG');
 //                  } );
 //                  
-//                  $(#s1).click(function{
+//                  $('#s1').click(function{
 //                      clicked_val=1;
 //                  });
-//         $(#s2).hover(function{
+//         $('#s2').hover(function{
 //                          
-//                          $(#s1).attr('src','Images/gstar.png');
-//                           $(#s1).attr('src','Images/gstar.png');
-//                            $(#s1).attr('src','Images/estar.png');
-//                             $(#s1).attr('src','Images/estar.png');
-//                              $(#s1).attr('src','Images/estar.png');
+//                          $('#s1').attr('src','Images/gstar.png');
+//                           $('#s2').attr('src','Images/gstar.png');
+//                            $('#s3').attr('src','Images/estar.JPG');
+//                             $('#s4').attr('src','Images/estar.JPG');
+//                              $('#s5').attr('src','Images/estar.JPG');
 //    } );
-//    $(#s1).click(function{
+//    $('#s2').click(function{
 //                      clicked_val=2;
 //                  });
-//         $(#s3).hover(function{
+//         $('#s3').hover(function{
 //                          
-//                          $(#s1).attr('src','Images/gstar.png');
-//                           $(#s1).attr('src','Images/gstar.png');
-//                            $(#s1).attr('src','Images/gstar.png');
-//                             $(#s1).attr('src','Images/estar.png');
-//                              $(#s1).attr('src','Images/estar.png');
+//                          $('#s1').attr('src','Images/gstar.png');
+//                           $('#s2').attr('src','Images/gstar.png');
+//                            $('#s3').attr('src','Images/gstar.png');
+//                             $('#s4').attr('src','Images/estar.JPG');
+//                              $('#s5').attr('src','Images/estar.JPG');
 //                          
 //    });
-//    $(#s1).click(function{
+//    $('#s3').click(function{
 //                      clicked_val=3;
 //                  });
-//         $(#s4).hover(function{
+//         $('#s4').hover(function{
 //                          
-//                          $(#s1).attr('src','Images/gstar.png');
-//                           $(#s1).attr('src','Images/gstar.png');
-//                            $(#s1).attr('src','Images/gstar.png');
-//                             $(#s1).attr('src','Images/gstar.png');
-//                              $(#s1).attr('src','Images/estar.png');
+//                          $('#s1').attr('src','Images/gstar.png');
+//                           $('#s2').attr('src','Images/gstar.png');
+//                            $('#s3').attr('src','Images/gstar.png');
+//                             $('#s4').attr('src','Images/gstar.png');
+//                              $('#s5').attr('src','Images/estar.JPG');
 //                          
 //    });
-//    $(#s1).click(function{
+//    $('#s4').click(function{
 //                      clicked_val=4;
 //                  });
-//         $(#s5).hover(function{
+//         $('#s5').hover(function{
 //                          
-//                          $(#s1).attr('src','Images/gstar.png');
-//                           $(#s1).attr('src','Images/gstar.png');
-//                            $(#s1).attr('src','Images/gstar.png');
-//                             $(#s1).attr('src','Images/gstar.png');
-//                              $(#s1).attr('src','Images/gstar.png');
+//                          $('#s1').attr('src','Images/gstar.png');
+//                           $('#s2').attr('src','Images/gstar.png');
+//                            $('#s3').attr('src','Images/gstar.png');
+//                             $('#s4').attr('src','Images/gstar.png');
+//                              $('#s5').attr('src','Images/gstar.png');
 //                          
 //    });
-//    $(#s1).click(function{
+//    $('#s5').click(function{
 //                      clicked_val=5;
 //                  });
 //           $('.div_star').mouseout(function{
-//                          $(#s1).attr('src','Images/estar.png');
-//                           $(#s1).attr('src','Images/estar.png');
-//                            $(#s1).attr('src','Images/estar.png');
-//                             $(#s1).attr('src','Images/estar.png');
-//                              $(#s1).attr('src','Images/estar.png');
+//                          $('#s1').attr('src','Images/estar.JPG');
+//                           $('#s2').attr('src','Images/estar.JPG');
+//                            $('#s3').attr('src','Images/estar.JPG');
+//                             $('#s4').attr('src','Images/estar.JPG');
+//                              $('#s5').attr('src','Images/estar.JPG');
 //               
 //               
 //           });
-    }
-            )
-            
+//    }
+//            
+//            );
+//            
             </script>
     </head>
     <body>
         <div class="div_cont">
             <div class="div_star">
-                <img src="Images/estar.JPG" class="star" id='s1' onmouseover="this.src='Images/gstar.png';" onmouseout="this.src='Images/estar.JPG';"/>
-        <img src="Images/estar.JPG" class="star" id="s2" onmouseover="this.src='Images/gstar.png';" onmouseout="this.src='Images/estar.JPG';"/>
-        <img src="Images/estar.JPG" class="star" id="s3" onmouseover="this.src='Images/gstar.png';" onmouseout="this.src='Images/estar.JPG';"/>
-        <img src="Images/estar.JPG" class="star" id="s4" onmouseover="this.src='Images/gstar.png';" onmouseout="this.src='Images/estar.JPG';"/>
-        <img src="Images/estar.JPG" class="star" id="s5" onmouseover="this.src='Images/gstar.png';" onmouseout="this.src='Images/estar.JPG';"/>
+                <img src="Images/estar.JPG" class="star" onmouseover="src='Images/gstar.png'" onmouseout="src='Images/estar.JPG'" id='s1' />
+                    <img src="Images/estar.JPG" class="star" id="s2" />
+                    <img src="Images/estar.JPG" class="star" id="s3" />
+                    <img src="Images/estar.JPG" class="star" id="s4" />
+                    <img src="Images/estar.JPG" class="star" id="s5" />
             </div>
         </div>
     </body>
